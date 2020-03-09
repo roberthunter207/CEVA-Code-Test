e1d148e3dee8b33891b803046f0ae4c7bad7f4cb
Robert Hunter <61474100+roberthunter207@users.noreply.github.com>
2020-03-09 18:20:11 +1100

    Add files via upload

---
+++ NumbersToWords.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEVACodeTest
+{
+    class NumbersToWords
+    {
+        private readonly string[] singleDigits = new[] { "", "One", "Two", "Three",
+                    "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten",
+                    "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen",
+                    "Sixteen", "Seventeen", "Eighteen", "Nineteen" };
+        private readonly string[] doubleDigits = new[] { "Ten", "Twenty", "Thirty",
+                    "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+        private readonly string[] largeNumNames = new[] { " Billion ", " Million ", 
+                    " Thousand ", " Hundred "};
+        public string IntegerToWords(long num)
+        {
+            string words = "";
+
+            /*
+             * This switch statement is used to check whether or not the User has
+             * only input the number '0'.
+             * If so, the value returned to the console will be "Zero", and the
+             * application will terminate here. However, if the input value is anything other
+             * than '0', the application will continue to process following the default
+             * case of the switch statement.
+             */
+            switch (num)
+            {
+                case 0:
+                    return "Zero";
+                default:
+                    return ConvertToWords();
+            }
+
+            /*
+             * This method's purpose to convert and input value other than '0'
+             * into words.
+             * The method is of recursive nature and works by first understanding 
+             * whether or not the value is negative. If so, the method will call 
+             * itself again to reiterate through the if statement to further convert 
+             * the input values into words and find whether or not it's necessary to
+             * add large number names such as 'Billion' or 'Thousand' to the string 'words'. 
+             * 
+             * The modulus operator (%) is used to grab the remainder of the input value
+             * to ensure that it is matched with it's correlating value or 'word' in the 
+             * 'singleDigits' or 'doubleDigits' array. Eg. The input value of '1' is equal to
+             * 'singleDigits[1]' thus returning 'One'.
+             */
+            string ConvertToWords()
+            {
+                if (num < 0)
+                {
+                    words += "Negative " + IntegerToWords((Math.Abs(num)));
+                }
+                if ((num / ((int)Math.Pow(10,9))) > 0)
+                {
+                    words += IntegerToWords(num / ((int)Math.Pow(10, 9))) + largeNumNames[0];
+                    num %= (int)Math.Pow(10,9);
+                }
+                if ((num / ((int)Math.Pow(10, 6))) > 0)
+                {
+                    words += IntegerToWords(num / ((int)Math.Pow(10, 6))) + largeNumNames[1];
+                    num %= (int)Math.Pow(10, 6);
+                }
+
+                if ((num / ((int)Math.Pow(10, 3))) > 0)
+                {
+                    words += IntegerToWords(num / ((int)Math.Pow(10, 3))) + largeNumNames[2];
+                    num %= (int)Math.Pow(10, 3);
+                }
+
+                if ((num / ((int)Math.Pow(10, 2))) > 0)
+                {
+                    words += IntegerToWords(num / ((int)Math.Pow(10, 2))) + largeNumNames[3];
+                    num %= (int)Math.Pow(10, 2);
+                }
+
+                if (num > 0)
+                {
+                    if (!string.IsNullOrEmpty(words))
+                        words += "And ";
+
+                    if (num < 20)
+                        words += singleDigits[num];
+                    else
+                    {
+                        words += doubleDigits[(num / 10) - 1];
+                        if ((num % 10) > 0)
+                            words += " " + singleDigits[num % 10];
+                    }
+                }
+                return words;
+            }
+        }
+    }
+}
---
+++ UserInput.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CEVACodeTest
+{
+    class UserInput
+    {
+        /*
+         * This method instantiates the NumberToWords class and retrieves user input.
+         * It retrieves the Date by utilising the DateTime struct and then converting to a 'Short'
+         * date string to eliminate the time portion.
+         * 
+         * The User is then asked to input a number of any value whether it's negative or positive.
+         * The tryParse method is used to ensure that the User does input an appropriate integer
+         * and not a string. If the User inputs anything other than integer values, they are
+         * responded with a message and are asked to input a value again until the condition
+         * of the 'do-while' loop is met.
+         */
+        public void GetUserInput()
+        {
+            NumbersToWords ConvertToWords = new NumbersToWords();
+            DateTime currentDateAndTime = DateTime.Now;
+            string currentDate = currentDateAndTime.ToShortDateString();
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
+            do
+            {
+                Console.Write("Enter an amount in numbers (Eg. 123): ");
+                string num = Console.ReadLine();
+                if (long.TryParse(num, out _))
+                {
+                    Console.WriteLine(name);
+                    Console.WriteLine(currentDate);
+                    Console.WriteLine(ConvertToWords.IntegerToWords(Convert.ToInt64(num)));
+                    Console.Read();
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect Input!");
+                }
+            } while (true);
+        }
+    }
+}
